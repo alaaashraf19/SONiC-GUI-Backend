f38518ddfee9d497f79ef5c3bf3de5cdcddd5282
Alaa <alaa.ashraf.atef@gmail.com>
2025-07-23 14:14:03 +0300

    removed unrealted files, fixed naming issues

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from app.routers.auth import router as auth_router
-from app.routers.port_oper import router as post_oper_router
 from app.routers.vlans import router as vlans_router
 
 app=FastAPI()
@@ -8,5 +7,4 @@ app=FastAPI()
 
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
-app.include_router(post_oper_router, prefix="/post_oper",tags=["post_oper"])
 app.include_router(vlans_router, prefix="/vlans",tags=["vlans"])
