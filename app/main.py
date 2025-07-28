6cd41fe6ce063ee59e2f3d64b92ee9fca8f1e78a
ranatamerr <roneytamer@gmail.com>
2025-07-28 12:58:12 +0300

    deleted unwanted files and merged endpoints into a single file

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ from app.routers.auth import router as auth_router
 from app.routers.post_oper import router as post_oper_router
 from app.routers.vlans import router as vlans_router
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers.PutOper import router as put_oper_vlan_router
-from app.routers.DeleteOper import delete_oper_vlan_router
+
 
 
 app=FastAPI()
@@ -20,8 +19,5 @@ app.add_middleware(
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(post_oper_router, prefix="/post_oper",tags=["post_oper"])
-app.include_router(vlans_router, prefix="/network",tags=["vlans"])
-app.include_router(put_oper_vlan_router, prefix="/network", tags=["vlans"])
-app.include_router(delete_oper_vlan_router, prefix="/network", tags=["vlans"])
-
+app.include_router(vlans_router, prefix="/vlans",tags=["vlans"])
 
--- app/routers/vlans.py
+++ app/routers/vlans.py
@@ -3,6 +3,11 @@ import httpx
 from dotenv import load_dotenv
 import os
 from app.services.sonic_services import fetch_vlans
+from app.models.vlan import VLan_request
+from app.services.put_services import put_vlan  
+from app.services.DeleteServices import delete_all_vlans_from_switch,delete_vlan_by_name,delete_vlan_description_by_name
+
+
 
 router = APIRouter()
 
@@ -19,3 +24,37 @@ SONIC_BASE_URL = os.getenv("SONIC_BASE_URL")
 @router.get("/vlans")
 async def get_vlans():
  return await fetch_vlans()   
+
+@router.put("/put", tags=["vlans"])
+async def update_vlan(payload: VLan_request):
+    try:
+        # Restructure it to match the switch's expected payload
+        formatted_payload = {
+            "sonic-vlan:sonic-vlan": {
+                "VLAN": payload.vlan.dict(by_alias=True),
+                "VLAN_MEMBER": payload.members.dict(by_alias=True)
+            }
+        }
+        return await put_vlan(formatted_payload)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.delete("/delete/all-vlan-config", summary="Delete ALL VLAN config (VLANs + Members)")
+async def delete_all_vlan_config():
+    try:
+        return await delete_all_vlans_from_switch()
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {exc}")
+    
+@router.delete("/delete/vlan/{vlan_name}", summary="Delete a single VLAN by name")
+async def delete_single_vlan(vlan_name: str):
+    return await delete_vlan_by_name(vlan_name)    
+
+
+@router.delete(
+    "/delete/vlan-description/{vlan_name}", summary="Delete VLAN description by name"
+)
+async def delete_vlan_description(vlan_name: str):
+    return await delete_vlan_description_by_name(vlan_name)    
